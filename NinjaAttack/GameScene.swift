b8618eccd7ed15645c496b0198c5efc59f06b3b9
jfk17a <jfk17a@acu.edu>
2020-02-04 14:54:05 -0600

    Changed LaunchScreen

--- NinjaAttack/GameScene.swift
+++ NinjaAttack/GameScene.swift
@@ -75,7 +75,7 @@ class GameScene: SKScene {
 
     
   override func didMove(to view: SKView) {
-    backgroundColor = SKColor.white
+    backgroundColor = SKColor.green
     player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
     addChild(player)
     
